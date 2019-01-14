51d4c23816dc392c49bbdf3f6bf89f5f3d3d67fd
Tyrone Damasceno <tyronedamasceno@gmail.com>
2019-01-14 10:21:59 -0300

    Django admin support create  users

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -32,3 +32,10 @@ class AdminSiteTests(TestCase):
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
+
+    def test_create_user_page(self):
+        """Test that the create user page works"""
+        url = reverse('admin:core_user_add')
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
